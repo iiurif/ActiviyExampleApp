6b6f03e5621632e84fb3adca959eecd9d870e7f5
iiurif <iurifdev@gmail.com>
2025-03-20 12:03:49 -0300

    feature : new onboarding

---
+++ ActiviyExampleApp/Screens/Onboarding/OnboardingPageFour.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPageFour.swift
+//  ActiviyExampleApp
+//
+//  Created by Iuri Ferreira on 20/03/25.
+//
+
+import SwiftUI
+
+struct OnboardingPageFour: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingPageFour()
+}
