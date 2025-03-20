8ce88efbfc953de2e62efa9979fee4e4ec44c78b
Iuri Ferreira <iurifdev@gmail.com>
2025-03-20 17:32:53 -0300

    add: new task screen

---
+++ ActiviyExampleApp/Screens/AddTask/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  ActiviyExampleApp
+//
+//  Created by User on 20/03/25.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
